26cf5c50a0933394177aebc90cba9b0033787270
Sergio David Vico Pineda <servicpin2@alu.edu.gva.es>
2024-01-31 18:53:04 +0100

    pagina notfound

---
+++ src/views/NotFound.vue
@@ -0,0 +1,15 @@
+<template>
+   <h1>Error 404</h1>
+   <picture>
+      <img class="mx-auto" src="https://http.cat/404" alt="404">
+   </picture>
+   <h2>No se ha encontrado la página.</h2>
+</template>
+<script>
+export default {
+   setup() {
+      document.title = "Error 404";
+   },
+   name: 'NotFound'
+}
+</script>
