cd7d740fd7aff67d9599b2e2f69f7d1bd482363d
Sergio David Vico Pineda <servicpin2@alu.edu.gva.es>
2024-01-31 18:52:04 +0100

    ruta Pokedex

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,8 @@
       <router-link to="/">Home</router-link> |
       <router-link to="/google">Google</router-link> |
       <router-link to="/youtube">Youtube</router-link> |
-      <router-link to="/about">About</router-link>
+      <router-link to="/about">About</router-link> |
+      <router-link to="/pokedex/1">Pokedex</router-link>
    </nav>
    <router-view />
 </template>
